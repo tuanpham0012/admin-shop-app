237c5a70aecde849864458e404ce784c0ec123db
Phạm Quốc Tuấn <tuanpham0012@gmail.com>
2024-10-10 13:52:08 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/admin-shop-app/',
   plugins: [
     vue(),
     vueJsx(),
