98f755ff8ffb5fc5687760b2b613a83570cfa970
Phạm Quốc Tuấn <tuanpham0012@gmail.com>
2024-10-11 15:08:10 +0700

    gi action

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,64 @@
-#!/user/bin/env sh
+name: Deploy VueJS Project
 
-set -e
+on:
+  push:
+    branches:
+      - main
 
-npm run build
+jobs:
+  build:
+    name: Build
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout Repo  # Checks out the repository.
+        uses: actions/checkout@v4
 
-cd dist
+      - name: Create Node Environment  # Sets up Node.js environment.
+        uses: actions/setup-node@v4
+        with: 
+          node-version: 20.x
+      - name: Install Dependencies
+        run: npm install
+      - name: Build App
+        run: npm run build -- --base=/admin-shop-app/
+      - name: Setup Pages
+        uses: actions/configure-pages@v4
+      - name: Archive artifact
+        shell: sh
+        run: |
+          tar \
+            --dereference --hard-dereference \
+            --directory "dist" \
+            -cvf "$RUNNER_TEMP/artifact.tar" \
+            --exclude=.git \
+            --exclude=.github \
+            .
+      - name: Upload artifact
+        uses: actions/upload-artifact@v4
+        with:
+          name: github-pages
+          path: ${{ runner.temp }}/artifact.tar
+          if-no-files-found: error
 
-git init
-git add -A
-git commit -m "New Deplotment"
-git push -f git@github.com:tuanpham0012/admin-shop-app.git main:gh-pages
+  deploy:
+    name: Deployment
+    # Chờ "build" xong mới chạy "deploy"
+    needs: build
 
-cd -
+    # Grant GITHUB_TOKEN the permissions required to make a Pages deployment
+    permissions:
+      actions: read
+      contents: read
+      pages: write      # to deploy to Pages
+      id-token: write   # to verify the deployment originates from an appropriate source
+
+    # Deploy to the github-pages environment
+    environment:
+      name: github-pages
+      url: ${{ steps.deployment.outputs.page_url }}
+
+    runs-on: ubuntu-latest
+    steps:
+      - name: Deploy to GitHub Pages
+        id: deployment
+        uses: actions/deploy-pages@v4
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/admin-shop-app/',
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
